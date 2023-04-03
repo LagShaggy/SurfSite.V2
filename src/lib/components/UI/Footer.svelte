fbc38517968659adc489b4d59fcb1960caec5c7f
Filip Dombos <filip.dombos@gmail.com>
2023-04-03 11:35:20 +0200

    sync

---
+++ src/lib/components/UI/Footer.svelte
@@ -0,0 +1,12 @@
+<script>
+	console.log('Hello');
+</script>
+
+<div class="flex justify-around bg-slate-200 fixed bottom-0 w-full h-auto">
+	<div class="w-1/3">
+		company information Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad iusto quae
+		porro ipsum animi consequuntur odio natus vero architecto ut ratione illum repellat veritatis,
+		voluptatibus suscipit placeat sunt atque debitis.
+	</div>
+	<div class="w-1/3">company adress</div>
+</div>
