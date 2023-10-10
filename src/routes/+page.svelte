81b2a5129258079cb2459aa99b2e8596e98d75ce
Filip Dombos <filip.dombos@gmail.com>
2023-10-10 17:16:06 +0200

    reset

--- src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	export let img = '/oe26_logo.svg';
-</script>
-
-<a href="/home" class="mt-1 h-screen flex items-center justify-center">
-	<img src={img} alt="OE26" class="hover:opacity-100 opacity-70 object-cover h-full" />
-</a>
