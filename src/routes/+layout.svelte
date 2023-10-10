1bb2f889524422a936afde946b83be5601e68ded
Filip Dombos <filip.dombos@gmail.com>
2023-10-10 18:00:10 +0200

    add boxes as placeholders

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,11 @@
 </script>
 
 <slot />
+
+<div class="flex justify-center">
+	<img class="opacity-25 absolute -z-10" src="oe26_logo.svg" alt="OE26 logo" />
+</div>
+
+<div class="bg-blue-500 w-20 h-96" />
+
+<div class="rounded bg-red-500 w-20 h-96" />
